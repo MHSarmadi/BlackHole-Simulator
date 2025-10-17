32dd79facc1c399a743d8931f9295718f7338650
MHSarmadi <Programming.MHSarmadi@gmail.com>
2025-10-17 05:19:23 +0330

    The Most basic form of black hole simulation

--- main.go
+++ main.go
@@ -42,31 +42,95 @@ out vec4 color;
 uniform sampler2D tex;
 uniform mat3 camRot;
 uniform vec3 camPos;
-bool intersectBox(vec3 ro, vec3 rd, vec3 boxmin, vec3 boxmax) {
-    vec3 invRd = 1.0 / rd;
-    vec3 tbot = invRd * (boxmin - ro);
-    vec3 ttop = invRd * (boxmax - ro);
-    vec3 tmin = min(ttop, tbot);
-    vec3 tmax = max(ttop, tbot);
-    vec2 t = max(tmin.xx, tmin.yz);
-    float tnear = max(t.x, t.y);
-    t = min(tmax.xx, tmax.yz);
-    float tfar = min(t.x, t.y);
-    return tnear <= tfar && tfar > 0.0;
-}
 void main() {
     vec2 frag = uv * 2.0 - 1.0;
     float aspect = 800.0/600.0;
     vec3 localDir = vec3(frag.x * aspect * tan(radians(45.0)), frag.y * tan(radians(45.0)), -1.0);
     vec3 dir = normalize(camRot * localDir);
-    vec3 boxmin = vec3(-0.5, -0.5, -0.5);
-    vec3 boxmax = vec3(0.5, 0.5, 0.5);
-    if (intersectBox(camPos, dir, boxmin, boxmax)) {
-        color = vec4(1.0, 1.0, 1.0, 1.0);
+
+    float M = 1.0;
+    float rs = 2.0 * M;
+    float dtau = 0.005;
+    int N = 3000;
+    float max_r = 10000.0;
+
+    vec3 pos_cart = camPos;
+    float r = length(pos_cart);
+    float theta = acos(pos_cart.y / r);
+    float phi = atan(pos_cart.x, pos_cart.z);
+
+    vec3 hat_r = pos_cart / r;
+    vec3 hat_theta = vec3(cos(theta) * cos(phi), -sin(theta), cos(theta) * sin(phi));
+    vec3 hat_phi = vec3(-sin(phi), 0.0, cos(phi));
+
+    float d_r = dot(dir, hat_r);
+    float d_th = dot(dir, hat_theta);
+    float d_ph = dot(dir, hat_phi);
+
+    float alpha = 1.0 - rs / r;
+
+    float sin_theta = sin(theta);
+    if (abs(sin_theta) < 1e-6) {
+        d_ph = 0.0;
+    }
+
+    float kt = 1.0 / sqrt(alpha);
+    float kr = d_r * sqrt(alpha);
+    float kth = d_th / r;
+    float kph = d_ph / (r * sin_theta);
+
+    vec4 x = vec4(0.0, r, theta, phi);
+    vec4 k = vec4(kt, kr, kth, kph);
+
+    bool hit = false;
+
+    for (int i = 0; i < N; i++) {
+        float curr_r = x.y;
+        if (curr_r < rs) {
+            hit = true;
+            break;
+        }
+        if (curr_r > max_r) {
+            break;
+        }
+        float sinth = sin(x.z);
+        float costh = cos(x.z);
+        float sin2 = sinth * sinth;
+        float Mr2 = M / (curr_r * curr_r);
+        alpha = 1.0 - rs / curr_r;
+        float gamma = 1.0 / alpha;
+
+        vec4 a = vec4(0.0);
+        a.x = -2.0 * (Mr2 * gamma) * k.y * k.x;
+        a.y = - (Mr2 * alpha) * k.x * k.x + (Mr2 * gamma) * k.y * k.y + curr_r * alpha * k.z * k.z + curr_r * alpha * sin2 * k.w * k.w;
+        if (abs(sinth) < 1e-6) {
+            a.z = - (2.0 / curr_r) * k.y * k.z;
+            a.w = - (2.0 / curr_r) * k.y * k.w;
+        } else {
+            float cotth = costh / sinth;
+            a.z = - (2.0 / curr_r) * k.y * k.z + sinth * costh * k.w * k.w;
+            a.w = - (2.0 / curr_r) * k.y * k.w - 2.0 * cotth * k.z * k.w;
+        }
+
+        x += dtau * k;
+        k += dtau * a;
+    }
+
+    if (hit) {
+        color = vec4(0.0, 0.0, 0.0, 1.0);
     } else {
-        float theta = acos(dir.y);
-        float phi = atan(dir.x, -dir.z);
-        vec2 sph = vec2((phi + 3.14159265) / (2.0 * 3.14159265), 1.0 - theta / 3.14159265);
+        float final_theta = x.z;
+        float final_phi = x.w;
+        float final_sinth = sin(final_theta);
+        float final_costh = cos(final_theta);
+        vec3 final_hat_r = vec3(final_sinth * cos(final_phi), final_costh, final_sinth * sin(final_phi));
+        vec3 final_hat_theta = vec3(final_costh * cos(final_phi), -final_sinth, final_costh * sin(final_phi));
+        vec3 final_hat_phi = vec3(-sin(final_phi), 0.0, cos(final_phi));
+        vec3 v = k.y * final_hat_r + x.y * k.z * final_hat_theta + x.y * final_sinth * k.w * final_hat_phi;
+        vec3 final_dir = normalize(v);
+        float final_theta_samp = acos(final_dir.y);
+        float final_phi_samp = atan(final_dir.x, -final_dir.z);
+        vec2 sph = vec2((final_phi_samp + 3.14159265) / (2.0 * 3.14159265), 1.0 - final_theta_samp / 3.14159265);
         color = texture(tex, sph);
     }
 }`
@@ -188,11 +252,31 @@ func createProgram(vs, fs string) uint32 {
 	gl.ShaderSource(vShader, 1, cstr, nil)
 	free()
 	gl.CompileShader(vShader)
+	var vStatus int32
+	gl.GetShaderiv(vShader, gl.COMPILE_STATUS, &vStatus)
+	if vStatus == gl.FALSE {
+		var logLen int32
+		gl.GetShaderiv(vShader, gl.INFO_LOG_LENGTH, &logLen)
+		log := make([]byte, logLen)
+		gl.GetShaderInfoLog(vShader, logLen, nil, &log[0])
+		panic(fmt.Sprintf("Vertex shader error: %s", log))
+	}
+
 	fShader := gl.CreateShader(gl.FRAGMENT_SHADER)
 	cstr, free = gl.Strs(fs)
 	gl.ShaderSource(fShader, 1, cstr, nil)
 	free()
 	gl.CompileShader(fShader)
+	var fStatus int32
+	gl.GetShaderiv(fShader, gl.COMPILE_STATUS, &fStatus)
+	if fStatus == gl.FALSE {
+		var logLen int32
+		gl.GetShaderiv(fShader, gl.INFO_LOG_LENGTH, &logLen)
+		log := make([]byte, logLen)
+		gl.GetShaderInfoLog(fShader, logLen, nil, &log[0])
+		panic(fmt.Sprintf("Fragment shader error: %s", log))
+	}
+
 	prog := gl.CreateProgram()
 	gl.AttachShader(prog, vShader)
 	gl.AttachShader(prog, fShader)
